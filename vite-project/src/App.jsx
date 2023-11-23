52a089f1de46b1e515e5cde2490597491e80acb9
huangrunhong <huang.run.hong.1986@gmail.com>
2023-11-23 20:33:39 +0100

    added online fonts

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-
 import Nav from './assets/components/Nav'
 import Header from './assets/components/Header'
 import Main from './assets/components/Main'
@@ -7,8 +5,7 @@ import Footer from './assets/components/Footer'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
   <Nav/>
